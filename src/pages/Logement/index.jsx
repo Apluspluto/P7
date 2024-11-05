f016cc196d703440db7bd5d769ef2cdc1f740dd7
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-05 11:51:19 +0100

    Ajout test pour voir si quand je clique sur une location j'ai bien les informations lié à celle-ci

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,5 +1,22 @@
-function Logement() {
-    return <div>Logement</div>
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function Logement({ data }) {
+  const { id } = useParams();
+  const location = data.find(location => location.id === id);
+
+  if (!location) {
+    return <p>Location non trouvée</p>;
   }
-  
-  export default Logement
+
+  return (
+    <div>
+      <h1>{location.title}</h1>
+      <img src={location.cover} alt={location.title} />
+      <p>{location.description}</p>
+      {/* Ce ne sont pas du tout les éléments définitif, c'est pour voir si les éléments s'affiche bien */}
+    </div>
+  );
+}
+
+export default Logement;
