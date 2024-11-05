bf51f799ed571793025814ef2cb38689a82862e8
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-05 11:41:14 +0100

    Ajout de mon data.json, pour pouvoir le passer en props aux éléments qui en ont besoin + changement de la route logement et Home

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/'
-import Logement from './pages/Home/Logement'
-import Propos from './pages/Home/Propos'
-import Error from './pages/Home/Error'
+import Home from './pages/Home'
+import Logement from './pages/Logement'
+import Propos from './pages/Propos'
+import Error from './pages/Error'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import data from '../src/data/data.json'
+import '../src/styles/pages/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <main>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Logement" element={<Logement />} />
+        <Route path="/" element={<Home data={data} />} />
+        <Route path="/Logement/:id" element={<Logement data={data} />} />
         <Route path='/Propos' element={<Propos/>} />
         <Route path="*" element={<Error />} />
       </Routes>
+      </main>
+      <Footer />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
