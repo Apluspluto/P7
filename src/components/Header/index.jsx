11748aad57773450085a0a2a6222bc07635bc55f
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-05 11:35:10 +0100

    Ajout du logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom'
- 
+import kasa from '../../assets/images/kasa.png';
+import '../../styles/components/header.scss';
+
 function Header() {
     return (
+        <header>
+        <div>
+        <img src={kasa} alt='logo kasa'/>
+        </div>
         <nav>
             <Link to="/">Accueil</Link>
             <Link to="/Propos">A propos</Link>
         </nav>
+        </header>
     )
 }
 
