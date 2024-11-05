43dc86ae72df82026cec73f6ac088ce9976e54ef
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-05 11:46:18 +0100

    Ajout de mon background + les locations avec mon composant location + data en props

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,17 @@
-function Home() {
-  return <div>Page d'accueil 🏡</div>
+import '../../styles/pages/home.scss'
+import fond from '../../assets/images/background.png'
+import Location from '../../components/Location';
+
+function Home({data}) {
+  return ( 
+    <section>
+    <div className='container_fond'>
+        <img src={fond} alt='background' className="home_fond"/>
+        <p className='home_titre'>Chez vous, partout et ailleurs.</p>
+    </div>
+    <Location data={data} />
+    </section>
+  ) 
 }
 
 export default Home
