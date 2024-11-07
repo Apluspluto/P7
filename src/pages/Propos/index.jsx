aefd1768d274a78ce45a63cc6f59819b41260b2c
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-07 16:36:18 +0100

    Ajout de collapse et modification du text + ajout du composant background.

--- src/pages/Propos/index.jsx
+++ src/pages/Propos/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import fond from '../../assets/images/kalen-emsley.png'
+import { Collapse } from 'react-collapse';
+import fond from '../../assets/images/kalen-emsley.png';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import '../../styles/pages/propos.scss'; 
+import '../../styles/pages/propos.scss';
+import Background from '../../components/Background';
 
 const Accordion = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -11,33 +13,33 @@ const Accordion = () => {
   };
 
   const sections = [
-    { title: 'Fiabilité', content: 'Détails sur la fiabilité...' },
-    { title: 'Respect', content: 'Détails sur le respect...' },
-    { title: 'Service', content: 'Détails sur le service...' },
-    { title: 'Sécurité', content: 'Détails sur la sécurité...' },
+    { title: 'Fiabilité', content: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.' },
+    { title: 'Respect', content: '  La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.' },
+    { title: 'Service', content: ' La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.' },
+    { title: 'Sécurité', content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique de nos hôtes." }
   ];
 
   return (
     <div>
-    <div className='container_fond'><img src={fond} alt='background' className="about-fond"/></div>
-    <div className="accordion">
-      {sections.map((section, index) => (
-        <div key={index} className="accordion-item">
-          <button
-            className="accordion-title"
-            onClick={() => handleToggle(index)}
-          >
-            {section.title}
-            <i className={`fa-solid ${activeIndex === index ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
-          </button>
-          {activeIndex === index && (
-            <div className="accordion-content">
-              {section.content}
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+      <Background image={fond} className="about-fond" />
+      <div className="accordion">
+        {sections.map((section, index) => (
+          <div key={index} className="accordion-item">
+            <button
+              className="accordion-title"
+              onClick={() => handleToggle(index)}
+            >
+              {section.title}
+              <i className={`fa-solid ${activeIndex === index ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+            </button>
+            <Collapse isOpened={activeIndex === index}>
+              <div className="accordion-content">
+                {section.content}
+              </div>
+            </Collapse>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
