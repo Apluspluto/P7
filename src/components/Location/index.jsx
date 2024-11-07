068bdf31ce5230877641186a91c31caa807d759a
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-07 16:27:19 +0100

    Ajout des props pour que mon composant soit réutilisable

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import '../../styles/components/location.scss';
 import { Link } from 'react-router-dom';
 
-function Location({ data }) {
+function Location({ id, title, cover, location }) {
   return (
-    <div className='container_location'>
-      {data.map(location => (
-        <Link key={location.id} to={`/Logement/${location.id}`}>
-          <div>
-            <img src={location.cover} alt={location.title} />
-            <p>{location.title}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
+    <Link key={id} to={`/Logement/${id}`} className="location-link">
+      <div className="location-card">
+        <img src={cover} alt={title} className="location-image" />
+        <div className="text-overlay">
+          <h1 className="location-title">{title}</h1>
+          <p className="location-description">{location}</p>
+        </div>
+      </div>
+    </Link>
   );
 }
 
