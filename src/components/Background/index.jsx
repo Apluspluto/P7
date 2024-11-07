4e6e8ec965e093c13504cee540fe2f1a75eb37c7
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-07 16:22:34 +0100

    Création du composant Background pour etre réutilisable.

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,12 @@
+import '../../styles/pages/home.scss';
+
+function Background({ image, title, className }) {
+  return ( 
+    <div className='container_fond'>
+        <img src={image} alt='background' className={className}/>
+        <p className='home_titre'>{title}</p>
+    </div>
+  );
+}
+
+export default Background;
