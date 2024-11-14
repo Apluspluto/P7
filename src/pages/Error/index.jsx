ad83338e38562f61f8b0674b11f80a2a09e732fb
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-14 13:51:50 +0100

    Ajout du titre et paragraphe

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,8 @@
 function Error() {
-    return <div>Error</div>
+    return <div className="container_error">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+    </div>
   }
   
   export default Error
