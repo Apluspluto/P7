60d562056d85396734e2332d0bdae4bcd928e0fd
Apluspluto <150562165+Apluspluto@users.noreply.github.com>
2024-11-14 13:25:10 +0100

    Modification du chemin vers mon scss

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import kasa from '../../assets/images/kasa.png';
-import '../../styles/components/header.scss';
+import '../Header/header.scss'
 
 function Header() {
     return (
